024a5577b594a76df0b1bb87139836e105c34fea
amireddym <amireddy.m@northeastern.edu>
2022-04-04 17:55:07 -0400

    Added metric collection interval changes

--- springdemo/src/main/java/com/neu/csye6225/springdemo/SpringdemoApplication.java
+++ springdemo/src/main/java/com/neu/csye6225/springdemo/SpringdemoApplication.java
@@ -1,6 +1,5 @@
 package com.neu.csye6225.springdemo;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
