63e1cdb3e7f5ac5ecc10312ac4bbac923012264e
amireddym <amireddy.m@northeastern.edu>
2022-04-03 22:35:13 -0400

    Updated the cloud deploy script to rerun cloudwatch agent

--- codedeploy/start_latest_application_server.sh
+++ codedeploy/start_latest_application_server.sh
@@ -2,6 +2,7 @@
 
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a stop
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c -file:/tmp/amazon-cloudwatch-agent.json -s
+sudo systemctl start amazon-cloudwatch-agent.service
 
 sudo chmod 777 /tmp/springdemo-0.0.1-SNAPSHOT.jar
 sudo systemctl start application_boot.service
