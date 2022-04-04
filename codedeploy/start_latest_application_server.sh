bf8be9a4dacb62b7866055e0761cb4532fe918c9
amireddym <amireddy.m@northeastern.edu>
2022-04-04 19:48:16 -0400

    Fixed config file issues

--- codedeploy/start_latest_application_server.sh
+++ codedeploy/start_latest_application_server.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a stop
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c -file:/tmp/amazon-cloudwatch-agent.json -s
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/tmp/amazon-cloudwatch-agent.json -s
 sudo systemctl start amazon-cloudwatch-agent.service
-
+sleep 5
 sudo chmod 777 /tmp/springdemo-0.0.1-SNAPSHOT.jar
 sudo systemctl start application_boot.service
