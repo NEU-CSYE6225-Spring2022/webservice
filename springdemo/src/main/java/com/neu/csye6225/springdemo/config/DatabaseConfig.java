83b0643a58d8d5c265003a742d4bd12fdb6c1760
amireddym <amireddy.m@northeastern.edu>
2022-04-21 12:08:26 -0400

    Added ssl changes to app for RDS connection

--- springdemo/src/main/java/com/neu/csye6225/springdemo/config/DatabaseConfig.java
+++ springdemo/src/main/java/com/neu/csye6225/springdemo/config/DatabaseConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.io.File;
 
 @Configuration
 public class DatabaseConfig {
@@ -26,10 +27,13 @@ public class DatabaseConfig {
     @Bean
     public DataSource getDataSource()
     {
+        String certiifcate = new File("src/main/resources/rds-combined-ca-bundle.pem").getAbsolutePath();
+
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
         if(database.equalsIgnoreCase(Constants.MYSQL)){
             dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
-            dataSourceBuilder.url("jdbc:mysql://"+ databasehost +"/csye6225?useSSL=true&serverTimezone=UTC&useLegacyDatetimeCode=false&allowPublicKeyRetrieval=true&sslmode=REQUIRED");
+            dataSourceBuilder.url("jdbc:mysql://"+ databasehost +"/csye6225?useSSL=true&serverTimezone=UTC&useLegacyDatetimeCode=false&allowPublicKeyRetrieval=true&sslmode=VERIFY_CA"
+                +"&sslrootcert="+ certiifcate);
         }else{
             dataSourceBuilder.driverClassName("org.h2.Driver");
             dataSourceBuilder.url("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
